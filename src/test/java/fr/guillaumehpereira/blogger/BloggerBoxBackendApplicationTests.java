9f530db27276abf66eeefba76692d41dde716ff6
guillaumepereira <hpereira.guillaume@gmail.com>
2024-04-22 14:25:57 +0200

    initialisation du back

---
+++ src/test/java/fr/guillaumehpereira/blogger/BloggerBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.guillaumehpereira.blogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
